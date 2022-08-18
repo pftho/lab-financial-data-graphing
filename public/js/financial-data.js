34ec7b2ea084669c8a5b0acaaba68d40a0ad5a95
Pauline Thomas <pauline.farina90@gmail.com>
2022-08-18 19:09:09 +0200

    done

--- public/js/financial-data.js
+++ public/js/financial-data.js
@@ -1,16 +1,53 @@
-const apiUrl =
-  "https://api.coindesk.com/v1/bpi/historical/close.json?start=2016-12-01&end=2017-02-05&currency=usd";
+const fromDateInput = document.getElementById("fromDate");
+const toDateInput = document.getElementById("toDate");
 
-const currency = axios
-  .get(apiUrl)
-  .then((response) => {
-    console.log("data", response.data);
-    printChart(response.data);
-  })
-  .catch((err) => console.log(err));
+function drawData() {
+  const apiUrl =
+    "https://api.coindesk.com/v1/bpi/historical/close.json?start=2016-12-01&end=2017-02-05&currency=usd";
+
+  axios
+    .get(apiUrl)
+    .then((response) => {
+      // console.log("data", response.data);
+      printChart(response.data);
+    })
+    .catch((err) => console.log(err));
+}
+
+drawData();
+
+const submitBtn = document.getElementById("updateData");
+
+
+function updateData() {
+  console.log("running update dataaa");
+  let currency = "usd";
+  let fromDate = fromDateInput.value;
+  let toDate = toDateInput.value;
+
+  console.log(fromDateInput, toDateInput);
+  console.log("Initial fromDate", fromDate, "Initial toDate", fromDate);
+
+  const updateApiUrl = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${fromDate}&end=${toDate}&currency=${currency}`;
+
+  console.log(updateApiUrl);
+
+  axios
+    .get(updateApiUrl)
+    .then((response) => {
+      // console.log("data", response.data);
+      printChart(response.data);
+    })
+    .catch((err) => console.log(err));
+}
+
+updateData();
+
+// call the function again with the function
+//destroy
 
 function printChart(dataToPrint) {
-  console.log(dataToPrint);
+  //  console.log(dataToPrint);
   const dates = Object.keys(dataToPrint.bpi);
   const bpi = dates.map((date) => dataToPrint.bpi[date]);
 
@@ -30,3 +67,21 @@ function printChart(dataToPrint) {
     },
   });
 }
+
+// DOM
+
+submitBtn.addEventListener("click", () => {
+    console.log("running");
+    updateData();
+  });
+  
+  fromDateInput.addEventListener("change", (e) => {
+    fromDate = fromDateInput.value;
+    console.log("Updated fromDate", fromDate);
+  });
+  
+  toDateInput.addEventListener("change", (e) => {
+    toDate = toDateInput.value;
+    console.log("Updated toDate", toDate);
+  });
+  
